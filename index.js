90942cd3ad74ea740b3205a6708b372384515e35
Judy Wu <jingjingwu2011@gmail.com>
2019-09-01 12:53:43 -0700

    test 3

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ var Routes = require('./route');
 server.route(Routes);
 
 server.start(function() {
-    console.log('Server test running at:', server.info.uri);
+    console.log('Server 1 test running at:', server.info.uri);
 })
